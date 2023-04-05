c603abf8dcd4abe9a512197157bb273dbc36a798
Vasily Postnicov <shamaz.mazum@gmail.com>
2023-04-05 16:20:35 +0300

    Upldate plots with disks

--- scripts&data/calc-disk.jl
+++ scripts&data/calc-disk.jl
@@ -1,8 +1,17 @@
 module Torquatsky
 using DelimitedFiles
+using Images
 import CorrelationFunctions.Map as M
 import CorrelationFunctions.Utilities as U
 export do_it!
+
+struct FuckKernel <: U.AbstractKernel
+end
+
+const foo = [1 1 1; 1 -8 1; 1 1 1]
+
+U.extract_edges(array :: AbstractArray, filter :: FuckKernel, topology :: U.AbstractTopology) =
+    abs.(imfilter(array, centered(foo / 6), U.edge2pad(topology)))
 
 function draw_ball(s, r)
     array = falses(s)
@@ -20,13 +29,12 @@ end
 
 function do_it!()
     R = 0.0334
-    flt = U.EdgeFilter(U.BCPeriodic(), U.Kernel3x3())
     for side in 100:100:3100
         disk = draw_ball((side, side), R*side)
         println(side)
         println(U.lowfreq_energy_ratio(disk))
 
-        ss = M.surfsurf(disk, false; periodic = true, filter = flt) |> M.average_directions
+        ss = M.surf2(disk, false; periodic = true, filter = U.ConvKernel(7)) |> M.average_directions
 
         open("disks-$(side)-we-ss.dat", "w") do out
             writedlm(out, ss)
