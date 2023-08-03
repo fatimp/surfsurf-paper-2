50eb73d75fb94483b674d2bfd62ad4280153ff75
Vasily Postnicov <shamaz.mazum@gmail.com>
2023-08-03 14:16:41 +0300

    Update plots (blobs)

--- scripts&data/splice.sh
+++ scripts&data/splice.sh
@@ -20,20 +20,38 @@ arrow_head="path 'M 0,0 l -15,-5  +5,+5  -5,+5  +15,-5 z'"
 #        -draw "fill black translate 2700,100 rotate -30 scale 2,2 $arrow_head" \
 #        ../images/fss-disk-7x7-final.png
 
-montage ../images/fss-blob-7x7.png ../images/fss-blob-7x7-inset.png -tile 2x1 -geometry +0+0 tmp.png
-convert tmp.png -stroke black -strokewidth 3 -fill none -gravity center \
-        -draw "rectangle 1350,1050 2350,1150" \
-        -draw "line 2350,1150 2700,1200" \
-        -draw "line 2350,1050 2700,100" \
-        -draw "fill black translate 2700,1200 rotate 10 scale 2,2 $arrow_head" \
-        -draw "fill black translate 2700,100 rotate -70 scale 2,2 $arrow_head" \
-        ../images/fss-blob-7x7-final.png
+#montage ../images/fss-blob-7x7.png ../images/fss-blob-7x7-inset.png -tile 2x1 -geometry +0+0 tmp.png
+#convert tmp.png -stroke black -strokewidth 3 -fill none -gravity center \
+#        -draw "rectangle 1350,1050 2350,1150" \
+#        -draw "line 2350,1150 2700,1200" \
+#        -draw "line 2350,1050 2700,100" \
+#        -draw "fill black translate 2700,1200 rotate 10 scale 2,2 $arrow_head" \
+#        -draw "fill black translate 2700,100 rotate -70 scale 2,2 $arrow_head" \
+#        ../images/fss-blob-7x7-final.png
+
+#montage ../images/fss-blob-3x3.png ../images/fss-blob-3x3-inset.png -tile 2x1 -geometry +0+0 tmp.png
+#convert tmp.png -stroke black -strokewidth 3 -fill none -gravity center \
+#        -draw "rectangle 1350,1050 2350,1150" \
+#        -draw "line 2350,1150 2700,1200" \
+#        -draw "line 2350,1050 2700,100" \
+#        -draw "fill black translate 2700,1200 rotate 10 scale 2,2 $arrow_head" \
+#        -draw "fill black translate 2700,100 rotate -70 scale 2,2 $arrow_head" \
+#        ../images/fss-blob-3x3-final.png
 
 montage ../images/fss-blob-3x3.png ../images/fss-blob-3x3-inset.png -tile 2x1 -geometry +0+0 tmp.png
 convert tmp.png -stroke black -strokewidth 3 -fill none -gravity center \
-        -draw "rectangle 1350,1050 2350,1150" \
-        -draw "line 2350,1150 2700,1200" \
-        -draw "line 2350,1050 2700,100" \
+        -draw "rectangle 1200,1050 2200,1150" \
+        -draw "line 2200,1150 2700,1200" \
+        -draw "line 2200,1050 2700,100" \
         -draw "fill black translate 2700,1200 rotate 10 scale 2,2 $arrow_head" \
         -draw "fill black translate 2700,100 rotate -70 scale 2,2 $arrow_head" \
         ../images/fss-blob-3x3-final.png
+
+montage ../images/fss-blob-7x7.png ../images/fss-blob-7x7-inset.png -tile 2x1 -geometry +0+0 tmp.png
+convert tmp.png -stroke black -strokewidth 3 -fill none -gravity center \
+        -draw "rectangle 1200,1050 2200,1150" \
+        -draw "line 2200,1150 2700,1200" \
+        -draw "line 2200,1050 2700,100" \
+        -draw "fill black translate 2700,1200 rotate 10 scale 2,2 $arrow_head" \
+        -draw "fill black translate 2700,100 rotate -70 scale 2,2 $arrow_head" \
+        ../images/fss-blob-7x7-final.png
